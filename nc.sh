d129e5e16032ecba843a8accb91a9b1ca15bc6d3
Rajeev Thakur <rajeev.kumar@yahoo.com>
2019-01-06 21:00:53 -0800

    remote port forwarding

--- nc.sh
+++ nc.sh
@@ -16,5 +16,28 @@ nc localhost 8080
 netstat -ntpl
 
 
+# Server
 #!/bin/bash -x
 while true; do { echo -e "HTTP/1.1 200 OK\r\n$(date)\r\n\r\nhello from $(hostname) on $(date)" |  nc -vlp 8080; } done
+
+
+#Tunnel
+ssh -nNT -L 9000:localhost:8080 rajeevt@35.247.2.233 &
+
+#client connect
+nc localhost 9000
+
+
+
+#Remote port forwarding
+
+# Public ip: 35.247.2.233
+# Corp-Mac: localhost
+#Tunnel
+ssh -nNT -R 12022:localhost:22 rajeevt@35.247.2.233 &
+
+#ssh to Public
+ssh rajeevt@35.247.2.233
+
+#Connect to mac
+ssh -p 12022 rajthakur@localhost
